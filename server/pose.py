837e40981911acf705e23779ea5befe0972645c9
Andrew Lim <andrew@DN51uc0c.SUNet>
2016-03-07 15:11:48 -0800

    Restructured server code, basic auth, and beginning implementation of photo api

---
+++ server/pose.py
@@ -0,0 +1,28 @@
+import auth
+
+from flask import Blueprint, Flask, request
+
+pose_api = Blueprint("pose_api", __name__)
+
+pose = None # String representing tango location/rotation
+
+@pose_api.route("/pose/update", methods = ["POST"])
+@auth.requires_auth
+def pose_update():
+    global pose
+    if request.method == "POST":
+        data = request.form
+        print "[/pose/update]: received "  + data.get("pose", None)
+        pose = data["pose"]
+        print "[/pose/update]: pose is now " + pose
+    return ""
+
+@pose_api.route("/pose/current")
+@auth.requires_auth
+def pose_current():
+    if pose == None:
+        print "[/pose/current]: current pose is undefined"
+        return "The tango is currently not initialized."
+    else:
+        print "[/pose/current]: current pose is " + pose
+        return "The tango's current pose is: " + pose
--- server/server.py
+++ server/server.py
@@ -1,30 +1,17 @@
+import auth
+
 from flask import Flask, request
+
+from camera import camera_api
+from pose import pose_api
+
 app = Flask(__name__)
 
-pose = None # String representing tango location/rotation
+app.register_blueprint(pose_api)
 
 @app.route("/")
 def home():
     return "Hello, from Sparrow!"
-
-@app.route("/pose/update", methods = ["POST"])
-def pose_update():    
-    global pose
-    if request.method == "POST":
-        data = request.form
-        print "[/pose/update]: received "  + data.get("pose", None)
-        pose = data["pose"]
-        print "[/pose/update]: pose is now " + pose
-    return ""
-
-@app.route("/pose/current")
-def pose_current():
-    if pose == None:
-        print "[/pose/current]: current pose is undefined"
-        return "The tango is currently not initialized."
-    else:
-        print "[/pose/current]: current pose is " + pose
-        return "The tango's current pose is: " + pose
     
 if __name__ == "__main__":
     # DISABLE FOR PROD
