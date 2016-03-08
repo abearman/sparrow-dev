11019fe377fdd9778c6d2baaf912b60f893b4011
Amy Bearman <AmyBear1@DNab404683.stanford.edu>
2016-03-07 22:22:33 -0800

    Working on manual flight

--- drone_flight/simple_mission.py
+++ drone_flight/simple_mission.py
@@ -17,54 +17,79 @@ print "Connecting to vehicle on: 'tcp:127.0.0.1:5760'"
 vehicle = connect('tcp:127.0.0.1:5760', wait_ready=True)
 print "Connected to vehicle"
 
+def arm_and_takeoff(aTargetAltitude):
+    """
+    Arms vehicle and fly to aTargetAltitude.
+    """
 
-# Get the set of commands from the vehicle
-cmds = vehicle.commands
-cmds.download()
-cmds.wait_ready()
-print "Downloaded commands"
+    print "Basic pre-arm checks"
+    # Don't try to arm until autopilot is ready
+    while not vehicle.is_armable:
+        print " Waiting for vehicle to initialise..."
+        time.sleep(1)
 
-# Create and add commands
-cmd0=Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_MISSION_START, 0, 0, 0, 2, 0, 0, 0, 0, 0)
-#cmd1=Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 0, 1, 0, 0, 0, 0, 0, 0)
-cmd2=Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 0, 0, 0, 10)
-cmd3=Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, 10, 10, 10)
-#cmds.add(cmd1)
-cmds.add(cmd2)
-cmds.add(cmd3)
-print "Added first two commmands"
-cmds.add(cmd0)
-cmds.upload() # Send commands
-print "Uploaded commands"
+    print "Arming motors"
+    # Copter should arm in GUIDED mode
+    vehicle.mode    = VehicleMode("GUIDED")
+    vehicle.armed   = True
 
+    # Confirm vehicle armed before attempting to take off
+    while not vehicle.armed:
+        print " Waiting for arming..."
+        time.sleep(1)
 
-print "Basic pre-arm checks"
-# Don't try to arm until autopilot is ready
-while not vehicle.is_armable:
-  print " Waiting for vehicle to initialise..."
-  time.sleep(1)
+    print "Taking off!"
+    vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
 
-print "Arming motors"
-print "Mode: ", vehicle.mode.name
-#vehicle.mode  = VehicleMode("GUIDED")
-vehicle.armed = True
+    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
+    #  after Vehicle.simple_takeoff will execute immediately).
+    while True:
+        print " Altitude: ", vehicle.location.global_relative_frame.alt
+        #Break and return from function just below target altitude.
+        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95:
+            print "Reached target altitude"
+            break
+        time.sleep(1)
 
-# Confirm vehicle armed before attempting to take off
-while not vehicle.armed:
-  print " Waiting for arming..."
-  time.sleep(1)
-print "Armed"
+#arm_and_takeoff(50)
 
 
-# Set the vehicle into auto mode
-print dir(vehicle)
-vehicle.mode = VehicleMode("AUTO")
-print "Set to auto mode"
+def send_ned_velocity(velocity_x, velocity_y, velocity_z, duration):
+	msg = vehicle.message_factory.set_position_target_local_ned_encode(
+        0,       # time_boot_ms (not used)
+        0, 0,    # target system, target component
+     	mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED, # frame
+        0b0000111111000111, # type_mask (only speeds enabled)
+        0, 0, 0, # x, y, z positions (not used)
+        velocity_x, velocity_y, velocity_z, # x, y, z velocity in m/s
+        0, 0, 0, # x, y, z acceleration (not supported yet, ignored in GCS_Mavlink)
+        0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink)
 
-print "Mode: ", vehicle.mode
+	for x in range(0,duration):
+		vehicle.send_mavlink(msg)
+		print " Altitude: ", vehicle.location.global_relative_frame.alt
+		time.sleep(1)
+
+vehicle.mode = VehicleMode("SPORT")
+
+
+msg = vehicle.message_factory.command_long_encode(
+    0, 0,    # target_system, target_component
+    mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, #command
+    0, #confirmation
+    0,    # param 1, yaw in degrees
+    0,          # param 2, yaw speed deg/s
+    0,          # param 3, direction -1 ccw, 1 cw
+    0, # param 4, relative offset 1, absolute angle 0
+    0, 0, 1)    # param 5 ~ 7 not used
+# send command to vehicle
+vehicle.send_mavlink(msg)
+
+
+
+send_ned_velocity(0, 0, -5, 5)
 
 while True:
 	print " Altitude: ", vehicle.location.global_relative_frame.alt
 	print "Local location: ", vehicle.location.local_frame
-	#Break and return from function just below target altitude.
 	time.sleep(1)
