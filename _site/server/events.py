b7d5636e6fac06d9c92baea108fd6df4771352c0
Andrew Lim <andrew@Andrews-MacBook-Pro-92.local>
2016-03-11 21:49:28 -0800

    Latest Jekyll build of site

---
+++ _site/server/events.py
@@ -0,0 +1,38 @@
+# Events and associated callbacks for server-side sockets
+
+from flask import session
+from flask.ext.socketio import emit, join_room, leave_room
+from .. import socketio
+
+@socketio.on('pose_update', namespace='/pose')
+def pose_update(json):
+    print("[socket][pose_update]: " + str(json)
+
+
+def wide_angle_camera(
+
+
+@socketio.on('joined', namespace='/chat')
+def joined(message):
+    """Sent by clients when they enter a room.
+    A status message is broadcast to all people in the room."""
+    room = session.get('room')
+    join_room(room)
+    emit('status', {'msg': session.get('name') + ' has entered the room.'}, room=room)
+
+
+@socketio.on('text', namespace='/chat')
+def left(message):
+    """Sent by a client when the user entered a new message.
+    The message is sent to all people in the room."""
+    room = session.get('room')
+    emit('message', {'msg': session.get('name') + ':' + message['msg']}, room=room)
+
+
+@socketio.on('left', namespace='/chat')
+def left(message):
+    """Sent by clients when they leave a room.
+    A status message is broadcast to all people in the room."""
+    room = session.get('room')
+    leave_room(room)
+    emit('status', {'msg': session.get('name') + ' has left the room.'}, room=room)
