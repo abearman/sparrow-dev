5b37cf570db9ca8560d54ab7c2e3cc469d9fd5da
Andrew Lim <andrew@lim.org>
2016-03-12 19:06:19 -0800

    Schema for http request logging and MongoDB config for server

--- server/server.py
+++ server/server.py
@@ -1,12 +1,16 @@
 import auth
 
 from flask import Flask, request
+from flask.ext.mongoengine import MongoEngine
 from flask_socketio import SocketIO
 
 from camera import camera_api
 from pose import pose_api
 
 app = Flask(__name__)
+app.config["MONGODB_SETTINGS"] = {'DB': "sparrow_server_event_log"}
+
+db = MongoEngine(app)
 
 app.register_blueprint(pose_api)
 
