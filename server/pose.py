bb1a9256492b35fd522d72807cf5ce18ede714dd
Andrew Lim <andrew@lim.org>
2016-04-03 23:23:50 -0700

    Reformat pose endpoints to expect JSON rather than coordinate string

--- server/pose.py
+++ server/pose.py
@@ -1,22 +1,18 @@
 import auth
 
+from mission_data import path, pose
+
 from flask import Blueprint, Flask, request
 
 pose_api = Blueprint("pose_api", __name__)
-
-pose = None # String representing tango location/rotation
-
-path = None
 
 @pose_api.route("/pose/update", methods = ["POST"])
 # @auth.requires_auth
 def pose_update():
     global pose
     if request.method == "POST":
-        data = request.json
-        print "[/pose/update]: received "  + data["pose"]
-        pose = data["pose"]
-        print "[/pose/update]: pose is now " + pose
+        pose = request.json
+        print "[/pose/update]: pose is now " + str(pose)
     return ""
 
 @pose_api.route("/pose/current")
@@ -26,15 +22,15 @@ def pose_current():
         print "[/pose/current]: current pose is undefined"
         return "The tango is currently not initialized."
     else:
-        print "[/pose/current]: current pose is " + pose
-        return "The tango's current pose is: " + pose
+        print "[/pose/current]: current pose is " + str(pose)
+        return "The tango's current pose is: " + str(pose)
 
 @pose_api.route("/pose/path_config")
 def path_config():
     global path
     if request.method == "POST":
         data = request.json
-        print "[/pose/path_config]: received " + data["path"]
+        print "[/pose/path_config]: received " + str(data["path"])
         path = []
         for coord_tuple in data:
             path.append( (coord_tuple[0], coord_tuple[1]) )
