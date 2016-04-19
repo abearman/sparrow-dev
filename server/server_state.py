9dc2a191cee3560c7788ac5bf65f626ebdf6bee4
amybear <amy@hyperfine.com>
2016-04-18 22:29:58 -0700

    Made general direction getting methods

--- server/server_state.py
+++ server/server_state.py
@@ -18,6 +18,7 @@ db = MongoEngine(app)
 app.register_blueprint(pose_api)
 app.register_blueprint(monitor_api)
 
-HOST = "10.34.162.81" # This needs to be modified for server's IP    
+#HOST = "10.34.162.81" # This needs to be modified for server's IP    
+HOST = "localhost"
 
 socketio = SocketIO(app)
