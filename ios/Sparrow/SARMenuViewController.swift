0315dfdf03266dcb6421df7f803f0e49834aaa50
Pavitra Rengarajan <pavitra.t.rengarajan@gmail.com>
2016-05-02 23:04:32 -0700

    Added autonomous SAR path functionality

---
+++ ios/Sparrow/SARMenuViewController.swift
@@ -0,0 +1,53 @@
+//
+//  SARMenuViewController.swift
+//  Sparrow
+//
+//  Created by Pavitra Rengarajan on 5/2/16.
+//  Copyright © 2016 Andrew Lim. All rights reserved.
+//
+
+import Foundation
+
+class SARMenuViewController: UITableViewController {
+    
+    var delegate: DroneViewController? = nil
+    
+    var data = ["Line", "Radial", "Sector"]
+    var rowHeight = 40
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let height = data.count * Int(rowHeight) - 1
+        self.preferredContentSize = CGSize(width: 300, height: height)
+
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("sarCell", forIndexPath: indexPath)
+        cell.textLabel!.text = data[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return CGFloat(rowHeight)
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let selectedCell = tableView.cellForRowAtIndexPath(indexPath)!
+        print(selectedCell.textLabel)
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
