e54b002b16e398d22ab6727a34dbb7f3162f23a4
alim907 <andrew@lim.org>
2016-05-08 19:53:44 -0700

    Added lz4 dependency

--- server/install_dependencies.sh
+++ server/install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dep_ubuntu="eventlet==0.17.4 Flask==0.10.1 Flask-SocketIO==1.0 Flask-WTF==0.9.5 greenlet==0.4.2 
 						itsdangerous==0.24 Jinja2==2.8 MarkupSafe==0.23 python-engineio==0.7.1 python-socketio==0.6.1 six==1.10.0
-						Werkzeug==0.10.4 WTForms==1.0.5 flask-mongoengine Flask-JSGlue Pillow"
+						Werkzeug==0.10.4 WTForms==1.0.5 flask-mongoengine Flask-JSGlue Pillow lz4"
 
 length=$(echo $dep_ubuntu | wc -w)
 
