d59f2a101953507d7f0401f96ebba090a9f8890c
Catherine Dong <cdong@cs.stanford.edu>
2016-05-10 21:46:36 -0700

    CurrentLocationAnnotation.swift added

---
+++ ios/MapPath/MapPath/CurrentLocationAnnotation.swift
@@ -0,0 +1,20 @@
+//
+//  CurrentLocationAnnotation.swift
+//  MapPath
+//
+//  Created by Catherine Dong on 5/3/16.
+//  Copyright © 2016 Sparrow. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class CurrentLocationAnnotation: NSObject, MKAnnotation {
+    let coordinate: CLLocationCoordinate2D
+    
+    init(coordinate: CLLocationCoordinate2D) {
+        self.coordinate = coordinate
+        super.init()
+    }
+    
+}
